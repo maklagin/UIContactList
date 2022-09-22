9f6d313c31731fb335670b2f40b12bda2ab2bff9
maklagin <108239343+maklagin@users.noreply.github.com>
2022-09-22 14:48:01 +0300

    Initial Commit

---
+++ UIContactList/UIContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIContactListApp.swift
+//  UIContactList
+//
+//  Created by Максим Кулагин on 22.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct UIContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
